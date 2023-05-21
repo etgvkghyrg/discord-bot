394513c54ea31302b1cdd281a6384142e0d2e646
etgvkghyrg <115227053+etgvkghyrg@users.noreply.github.com>
2023-05-21 13:04:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Client, GatewayIntentBits, Partials, Collection} = require('discord.js');
+
 
 const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
 const {User, Message, GuilddMember, ThreadMember, Channel} = Partials;
+
+const {loadEvents} = require('./Handlers/eventsHandler');
+const {loadCommands} = require('./Handlers/commandHandler');
+
 
 const client = new Client({
     intents: [Guilds, GuildMembers, GuildMessages],
     partials: [User, Message, GuildMembers, ThreadMember],
 });
-
-client.once('ready',() => {
-    console.log('Ready!')
-})
-
+client.commands = new Collection();
 client.config = require('./config.json')
 
-client.login(client.config.token)
+client.login(client.config.token).then(() => {
+   loadEvents(client);
+   loadCommands(client)
+})
