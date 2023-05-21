3925809c11af1b420c0959037f36abfaeaef0201
etgvkghyrg <115227053+etgvkghyrg@users.noreply.github.com>
2023-05-21 14:19:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const { Client, GatewayIntentBits, Partials, Collection} = require('discord.js');
-
+const {Client, GatewayIntentBits, Partials, Collection} = require("discord.js");
 
 const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
-const {User, Message, GuilddMember, ThreadMember, Channel} = Partials;
+const {User, Message, GuildMember, ThreadMember, Channel} = Partials;
 
-const {loadEvents} = require('./Handlers/eventsHandler');
-const {loadCommands} = require('./Handlers/commandHandler');
-
+const {loadEvents} = require("./Handlers/eventHandler");
+const {loadCommands} = require("./Handlers/commandHandler");
 
 const client = new Client({
-    intents: [Guilds, GuildMembers, GuildMessages],
-    partials: [User, Message, GuildMembers, ThreadMember],
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
 });
-client.commands = new Collection();
-client.config = require('./config.json')
 
-client.login(client.config.token).then(() => {
-   loadEvents(client);
-   loadCommands(client)
-})
+client.commands = new Collection();
+client.config = require("./config.json");
+client.messages = require("./messages.json");
+
+client
+  .login(token)
+  .then(() => {
+    loadCommands(client);
+    loadEvents(client);
+  })
+  .catch((err) => console.log(err));
